3a678c86f5394da051c3b1e803abc42809a8df23
patkins2 <patkins789@gmail.com>
2025-11-19 00:44:26 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,10 @@ function About({ aboutText, personalInfo }) {
     <section id="about">
       <h2>About Me</h2>
       <div className="tooltip">
-        <a href={personalInfo.resumeFile} target="_blank" rel="noopener noreferrer">
+        <a href={`/${personalInfo.resumeFile}`} target="_blank" rel="noopener noreferrer">
           <img 
             className="wiggle" 
-            src="images/headshot.jpg" 
+            src="/images/headshot.jpg" 
             alt="Paul Atkins - Headshot" 
             style={{ width: '200px', height: 'auto', borderRadius: '50%', marginBottom: '20px' }}
           />
@@ -24,16 +24,16 @@ function About({ aboutText, personalInfo }) {
       {/* Social Media Icons */}
       <div>
         <a href={`mailto:${personalInfo.email}`}>
-          <img src="images/email_icon.png" alt="Email Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+          <img src="/images/email_icon.png" alt="Email Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
         </a>
         <a href={personalInfo.linkedin} target="_blank" rel="noopener noreferrer">
-          <img src="images/linkedin_icon.png" alt="LinkedIn Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+          <img src="/images/linkedin_icon.png" alt="LinkedIn Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
         </a>
         <a href={personalInfo.github} target="_blank" rel="noopener noreferrer">
-          <img src="images/github_icon.png" alt="Github Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+          <img src="/images/github_icon.png" alt="Github Icon" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
         </a>
         <a href={personalInfo.artstation} target="_blank" rel="noopener noreferrer">
-          <img src="images/artstation_icon.png" alt="ArtStation Icon" style={{ width: '50px', height: '50px' }} />
+          <img src="/images/artstation_icon.png" alt="ArtStation Icon" style={{ width: '50px', height: '50px' }} />
         </a>
       </div>
     </section>
